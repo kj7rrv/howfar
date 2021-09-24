ddcafb480ce25e310c8a19bdc8a5a4ab7e814461
Armen <armen138@pm.me>
2021-09-24 13:07:56 -0400

    Start binding UI elements

--- src/gps/tracker.py
+++ src/gps/tracker.py
@@ -1,28 +1,75 @@
-import gpsd_api
-import distance
+from . import gpsd_api
+from . import distance
 import time
 
-conn = gpsd_api.Connection()
-conn.enabled = True
 
-total_distance = 0
+class Tracker:
+    callbacks = {}
+    total_distance = 0
+    connection = False
+    tracking = False
+    acquired = False
+    last_point = 0
+    def __init__(self):
+        try:
+            self.connection = gpsd_api.Connection()
+            connection.enabled = True
+        except ConnectionRefusedError:
+            print("Unable to connect to gpsd")
 
-print('Waiting for location...')
+    def on_event(self, event_name, callback):
+        if event_name not in self.callbacks:
+            self.callbacks[event_name] = []
+        self.callbacks[event_name].append(callback)
 
-while True:
-    tpvs = conn.get_messages(['TPV'], clear=True)
-    if tpvs:
-        conn.clear_queue()
+    def emit_event(self, event_name, data):
+        if event_name in self.callbacks:
+            for callback in self.callbacks[event_name]:
+                callback(data)
+
+    def poll(self):
+        if not self.connection:
+            self.emit_event("error", "Unable to connect to gpsd")
+            return False
+        if not self.acquired:
+            tpvs = self.connection.get_messages(['TPV'], clear=True)
+            if tpvs:
+                self.connection.clear_queue()
+                self.last_point = tpvs[-1]
+                self.acquired = True
+                self.emit_event("acquired", last_point)
+        else:
+            tpvs = self.connection.get_messages(['TPV'], clear=True)
+            if tpvs:
+                meters = distance.distance([self.last_point] + tpvs)[0]
+                self.total_distance += meters
+                self.last_point = tpvs[-1]
+                self.emit_event("distance", self.total_distance)
+        return self.tracking
+
+def main():
+    conn = gpsd_api.Connection()
+    conn.enabled = True
+
+    total_distance = 0
+
+    while True:
+        tpvs = conn.get_messages(['TPV'], clear=True)
+        if tpvs:
+            conn.clear_queue()
+            last_point = tpvs[-1]
+            print('Acquired location.')
+            break
+
+    while True:
+        tpvs = conn.get_messages(['TPV'], clear=True)
+        if not tpvs:
+            continue
+        meters = distance.distance([last_point] + tpvs)[0]
+        total_distance += meters
         last_point = tpvs[-1]
-        print('Acquired location.')
-        break
+        print(round(total_distance*1000)/1000)
+        time.sleep(3)
 
-while True:
-    tpvs = conn.get_messages(['TPV'], clear=True)
-    if not tpvs:
-        continue
-    meters = distance.distance([last_point] + tpvs)[0]
-    total_distance += meters
-    last_point = tpvs[-1]
-    print(round(total_distance*1000)/1000)
-    time.sleep(3)
+if __name__ == "main":
+    main()
--- src/window.py
+++ src/window.py
@@ -1,14 +1,72 @@
 import gi
 gi.require_version('Handy', '1')
-from gi.repository import Gtk, Handy
+from gi.repository import Gtk, Handy, GLib
 
+from .gps.tracker import Tracker
 
 @Gtk.Template(resource_path='/com/github/linux-distance-tracker/window.ui')
 class LdtWindow(Gtk.ApplicationWindow):
     __gtype_name__ = 'LdtWindow'
+    # init LibHandy so it renders properly
     Handy.init()
 
-    # label = Gtk.Template.Child()
+    # Bind to UI elements
+    stats_avg_pace:Gtk.Label = Gtk.Template.Child()
+    stats_distance:Gtk.Label = Gtk.Template.Child()
+    stats_duration:Gtk.Label = Gtk.Template.Child()
+    stats_pace:Gtk.Label = Gtk.Template.Child()
+    start_button:Gtk.Button = Gtk.Template.Child()
+    error_message_revealer:Gtk.Revealer = Gtk.Template.Child()
+    error_message_label:Gtk.Label = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.tracker = Tracker()
+        self.tracker.on_event("acquire", self.on_gps_acquired)
+        self.tracker.on_event("pace", self.on_pace_update)
+        self.tracker.on_event("distance", self.on_distance_update)
+        self.tracker.on_event("error", self.on_gps_error)
+
+    def render_number(self, number):
+        return "{:.1f}".format(number)
+
+    def toggle_tracking(self):
+        if self.tracker.tracking:
+            self.stop_tracking()
+        else:
+            self.start_tracking()
+
+    def start_tracking(self):
+        self.tracker.tracking = True
+        self.start_button.set_label("Stop Tracking")
+        GLib.idle_add(self.tracker.poll)
+
+    def stop_tracking(self):
+        self.tracker.tracking = False
+        self.start_button.set_label("Start Tracking")
+
+    # GPS callbacks
+
+    def on_gps_error(self, data):
+        self.error_message_label.set_text(data)
+        self.error_message_revealer.set_reveal_child(True)
+        self.stop_tracking()
+
+    def on_gps_acquired(self, data):
+        print("gps acquired", data)
+
+    def on_pace_update(self, data):
+        self.stats_pace.set_text(self.render_number(data))
+
+    def on_distance_update(self, data):
+        self.stats_distance.set_text(self.render_number(data))
+
+    # GTK callbacks
+
+    @Gtk.Template.Callback()
+    def on_start_button_clicked(self, button, userdata=None):
+        self.toggle_tracking()
+
+    @Gtk.Template.Callback()
+    def on_error_message_close(self, item, userdata=None):
+        self.error_message_revealer.set_reveal_child(False)
